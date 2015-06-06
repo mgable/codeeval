e19c803987b93cd3ff72c5148220481883248e7b
Mark Gable <mgable43@gmail.com>
2015-06-06 15:19:33 -0700

    adding unique elements

---
+++ easy/unique_elements.js
@@ -0,0 +1,24 @@
+"use strict";
+
+var fs  = require("fs");
+fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) {
+	if (line !== "") {
+		console.info(getUniques(line).toString());
+	}
+});
+
+function getUniques(str){
+	var array = str.split(/,\s?/),
+		results = [],
+		cache = [];
+
+	results = array.filter(function(v,i,a){
+		return cache.indexOf(v) === -1 ? cache.push(v) : false;
+	});
+
+	results.forEach(function(v,i,a){
+		a[i] = parseInt(v,10);
+	});
+
+	return results.sort();
+}
