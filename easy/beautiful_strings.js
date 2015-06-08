82f34ad2afcf5287493dea909744214a05fc63b0
Mark Gable <mgable@marketshare.com>
2015-06-08 15:40:05 -0700

    refactored beautiful strings

--- easy/beautiful_strings.js
+++ easy/beautiful_strings.js
@@ -8,28 +8,19 @@ fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) 
 });
 
 function beautify(str){
-	var arr = str.split(""), 
-      values = {},
-      start = 26,
-      total = 0,
-      sums=[];
-  
-  arr.forEach(function(item){
-    if (/\w/.test(item)){
-      item = item.toLowerCase();
-      (values[item]) ? values[item]++ :  values[item] = 1;
-    }
-  });
-      
-  for (var prop in values){
-    sums.push(values[prop]);
-  }
-  
-  sums.sort(function(a,b){return b-a;});
-  
-  total = sums.reduce(function(a,b){
-    return a + (b * start--);
-  },0);
+	var arr = str.split(""),
+    sums = [],
+    start = 26,
+    cache = {};
 
-  return total;
+  arr.forEach(function(_item_){
+    var item = _item_.toLowerCase();
+    if (/\w/.test(item) && !cache[item]){
+      var re = new RegExp(item, "ig");
+      sums.push(str.match(re).length);
+      cache[item] = true;
+    } 
+  });
+
+  return sums.sort().reverse().reduce(function(a,b){return a + (b * start--)},0);
 }
