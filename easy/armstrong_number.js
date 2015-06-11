db8889ace882d20b59cc74c67ed8098b225eddc7
Mark Gable <mgable@marketshare.com>
2015-06-11 16:52:36 -0700

    updated

--- easy/armstrong_number.js
+++ easy/armstrong_number.js
@@ -10,5 +10,5 @@ fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) 
 function isArmstrong(num){
   var numArr = num.split("").map(function(item){return parseInt(item)}),
       pow = numArr.map(function(item){return Math.pow(item,num.length)}).reduce(function(a,b){return a + b},0);
-  return pow === parseInt(num);
+  return (pow === parseInt(num)) ? "True" : "False";
 }
