7b97ced5ed306c3ee9a9d75a04ac99e9b91db2e1
Mark Gable <mgable@marketshare.com>
2015-06-25 09:52:41 -0700

    another

---
+++ easy/mixed_content.js
@@ -0,0 +1,22 @@
+"use strict";
+var fs  = require("fs");
+fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) {
+    if (line !== "") {
+        console.info(parse(line));
+    }
+});
+
+function parse(str){
+  var arr = str.split(","),
+      words = [],
+      digits = [];
+  arr.forEach(function(item){
+    if (isNaN(parseInt(item))){
+      words.push(item)
+    } else {
+      digits.push(item);
+    }
+  });
+  
+  return words.join(",") + (words.length ? "|":"") + digits.join(",");
+}
