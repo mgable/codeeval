df673e74b0a11ff31c1ec2a48456fbb725f600b4
Mark Gable <mgable@marketshare.com>
2015-06-25 16:19:07 -0700

    added split the number

---
+++ easy/split_the_number.js
@@ -0,0 +1,20 @@
+"use strict";
+var fs  = require("fs");
+fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) {
+    if (line !== "") {
+        console.info(parse(line));
+    }
+});
+
+function parse(line){
+  var arr = line.split(" "),
+      num = arr[0],
+      pattern = arr[1],
+      formula = pattern.match(/(\w*)([\+\-])(\w*)/),
+      first = num.slice(0,formula[1].length),
+      second = num.slice(formula[1].length),
+      operator = formula[2];
+  
+  return (operator === "+") ? parseInt(first) + parseInt(second) : first - second;
+
+}
