d5ad714bb583238db4428624ce172b190d370799
Mark Gable <mgable@marketshare.com>
2015-06-29 15:56:40 -0700

    added decimal to binary

---
+++ moderate/decimal_to_binary.js
@@ -0,0 +1,11 @@
+"use strict";
+var fs  = require("fs");
+fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) {
+    if (line !== "") {
+        console.info(parse(line));
+    }
+});
+
+function parse(data){
+  return parseInt(data).toString(2);
+}
