19ebda3760d5e535071609fd057c09a7d8acb952
Mark Gable <mgable@marketshare.com>
2015-06-29 15:51:52 -0700

    added sum of integers

---
+++ moderate/sum_of_integers.js
@@ -0,0 +1,24 @@
+"use strict";
+var fs  = require("fs");
+fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) {
+    if (line !== "") {
+        console.info(parse(line));
+    }
+});
+
+function parse(data){
+  var arr = data.split(",").map(function(item){return parseInt(item)}),
+      results = [];
+  
+  arr.forEach(function(item, index){
+    for (var x = (index + 1), limit = (arr.length + 1); x < limit; x++){
+      results.push(sum(arr.slice(index,index + x)));
+    }
+  });
+  
+  return Math.max.apply(Math, results);
+}
+
+function sum (arr){
+  return arr.reduce(function(a,b){return a+b},0);
+}
