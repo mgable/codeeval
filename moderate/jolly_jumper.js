0b4dbd9411ce464f798e772438abd8433e2b93d5
Mark Gable <mgable@marketshare.com>
2015-07-01 15:12:48 -0700

    updated jolly jumper

---
+++ moderate/jolly_jumper.js
@@ -0,0 +1,19 @@
+"use strict";
+var fs  = require("fs");
+fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) {
+    if (line !== "") {
+        console.info(parse(line));
+    }
+});
+
+function parse(line){
+  var list = line.split(" "),
+	  last = list.length - 1,
+      isJolly = list.every(function(v,i,a){
+        var dif = Math.abs(v - a[i+1]).toString();
+
+        return (i < last) ? ( a.indexOf(dif) > -1 ? true : false) : true;
+      });
+
+  return isJolly ? "Jolly" : "Not jolly";
+}
