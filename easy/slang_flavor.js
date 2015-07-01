8a81a86e0ce6e623aff06b32f84ebd43156e9163
Mark Gable <mgable@marketshare.com>
2015-07-01 12:58:12 -0700

    added age distribution

--- easy/slang_flavor.js
+++ easy/slang_flavor.js
@@ -2,6 +2,7 @@
 var fs  = require("fs"), slang = [", yeah!", ", this is crazy, I tell ya.", ", can U believe this?",", eh?", ", aw yea.",", yo.", "? No way!", ". Awesome!"],
 	re = /[\.\?\!]/g,
 	count = 0,
+	pointer = -1,
     match;
 fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) {
     if (line !== "") {
@@ -11,7 +12,15 @@ fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) 
 
 function parse(data){
   var str = data.replace(re, function(item){
-    return  (count++ % 2 == 1) ? slang.shift() : item;
+    return  (count++ % 2 == 1) ? slang[getPointer()] : item;
   });
   return str;
+}
+
+function getPointer(){
+	if (pointer++ > slang.length){
+		pointer = 0;
+	}
+	console.info("the pointer is %s", pointer)
+	return pointer;
 }
