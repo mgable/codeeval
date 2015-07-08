b01ca78c9d08bb32e1221d2119d53301d8fc9c84
Mark Gable <mgable@marketshare.com>
2015-07-08 16:57:30 -0700

    added overlapping retangles

---
+++ moderate/overlapping_rectangles.js
@@ -0,0 +1,32 @@
+"use strict";
+var fs  = require("fs");
+fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) {
+    if (line !== "") {
+        console.info(/* Function */(line));
+    }
+});
+
+var data = "-3,3,-1,1,1,-1,3,-3";
+
+console.info(parse(data));
+
+function parse(line){
+  var arr = line.split(","),
+      point1 = {x: arr[0], y: arr[1], height: arr[2] - arr[0], width: arr[4] - arr[1]},
+      point2 = {x: arr[5], y: arr[6], height: , width: ""},
+      
+  return line;
+}
+
+
+
+
+
+function isOverlapping(a, b) {
+  return !(
+    ((a.y + a.height) < (b.y)) ||
+    (a.y > (b.y + b.height)) ||
+    ((a.x + a.width) < b.x) ||
+    (a.x > (b.x + b.width))
+  );
+}
