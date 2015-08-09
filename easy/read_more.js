b17be5f163a908e62d0e41b929c46651e981bcde
Mark Gable <mgable43@gmail.com>
2015-08-09 16:49:50 -0700

    read more at 97%

--- easy/read_more.js
+++ easy/read_more.js
@@ -2,12 +2,12 @@
 var fs  = require("fs");
 fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) {
     if (line !== "") {
-        console.info(readMore(line));
+        console.info(parse(line));
     }
 });
 
-function readMore(line){
-	if (line.length <= 55) { return line; }
-	var newLine = line.slice(0,39);
-	return newLine.replace(/[\s\-]\w*$/,"") + "... <Read More>";
+function parse(line){
+  if (line.length <= 55) return line;
+  var seg = line.slice(0,40).replace(/ ([\w-])*$/, "") + "... <Read More>";
+  return seg;
 }
