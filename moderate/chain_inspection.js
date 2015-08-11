c74cabd39947416c959e0f1e5392be69bc20f52c
Mark Gable <mgable43@gmail.com>
2015-08-11 07:00:57 -0700

    added chain inspection

--- moderate/chain_inspection.js
+++ moderate/chain_inspection.js
@@ -1,35 +1,37 @@
 "use strict";
 var fs  = require("fs");
+Chain.prototype.traverse = traverse;
+Chain.prototype.allLinksVisited = allLinksVisited;
+Chain.prototype.hasNext = hasNext;
+Chain.prototype.next = next;
+
 fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) {
     if (line !== "") {
-        console.info(/* Function */(line));
+        console.info(parse(line));
     }
 });
 
-var data = "4-2;BEGIN-3;3-4;2-END";
-//var data = "4-2;BEGIN-3;3-4;2-3"; //# BAD
-//var data = "BEGIN-3;4-3;3-4;2-END"; // # BAD
-//var data = "77-END;BEGIN-8;8-77;11-11"; // # BAD
-
-console.info(parse(data));
-
-var arr = [];
 function parse(line){
-  arr = line.split(";").map(function(v,i,a){
+  var arr = line.split(";").map(function(v,i,a){
     var temp = v.split("-"),
         name = temp[0],
         link = temp[1];
     return {name: name, link:link, visited:false};
   });
 
+  var chain = new Chain(arr);
+  
+  return chain.traverse() && chain.allLinksVisited() ? "GOOD" : "BAD";
+}
 
-  return traverse () && allLinksVisited() ? "GOOD" : "BAD";
+function Chain(data){
+  this.data = data;
 }
 
 function traverse(){
   var pointer = "BEGIN";
-  while(pointer && hasNext(pointer)){
-    var nextObj = next(pointer);
+  while(pointer && this.hasNext(pointer)){
+    var nextObj = this.next(pointer);
     pointer = nextObj.link;
     if (nextObj.visited) {return false;}
     nextObj.visited = true;
@@ -40,15 +42,15 @@ function traverse(){
 }
 
 function allLinksVisited(){
-  for (var x = 0; x < arr.length; x++){
-    if(!arr[x].visited) return false;
+  for (var x = 0; x < this.data.length; x++){
+    if(!this.data[x].visited) return false;
   }
   return true;
 }
 
 function hasNext(type){
-  for (var x = 0; x < arr.length; x++){
-    if (arr[x].name === type) {
+  for (var x = 0; x < this.data.length; x++){
+    if (this.data[x].name === type) {
       return true;
     }
   }
@@ -56,11 +58,10 @@ function hasNext(type){
 }
 
 function next(type){
-  for (var x = 0; x < arr.length; x++){
-    if (arr[x].name === type) {
-      return arr[x];
+  for (var x = 0; x < this.data.length; x++){
+    if (this.data[x].name === type) {
+      return this.data[x];
     }
   }
-  
   return null;
 }
