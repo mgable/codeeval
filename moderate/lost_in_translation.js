9d7e0d80de738465ff5a69ca1a2cc307636c74c3
Mark Gable <mgable43@gmail.com>
2015-08-15 10:21:31 -0700

    oops

--- moderate/lost_in_translation.js
+++ moderate/lost_in_translation.js
@@ -1,10 +1,11 @@
 "use strict";
 var fs  = require("fs");
 
-var example1 = "rbc vjnmkf kd yxyqci na rbc zjkfoscdd ew rbc ujllmcptc rbkso rbyr ejp mysljylc kd kxveddknmc re jsicpdryside kr kd eoya kw aej icfkici re zjkr;the public is amazed by the quickness of the juggler",
-	example2 = "tc rbkso rbyr ejp mysljylc kd kxveddknmc re jsicpdrysi;we think that our language is impossible to understand",
-	map = makeMap(example1, {});
+var example1 = "rbc vjnmkf kd yxyqci na rbc zjkfoscdd ew rbc ujllmcptc rbkso rbyr ejp mysljylc kd kxveddknmc re jsicpdryside kr kd eoya kw aej icfkici re zjkr;the public is amazed by the quickness of the juggler";
+var example2 = "tc rbkso rbyr ejp mysljylc kd kxveddknmc re jsicpdrysi;we think that our language is impossible to understand";
+var map= {"v": "g", "x": "h"};
 
+map = makeMap(example1,map);
 map = makeMap(example2, map);
 map = swap(map);
 
@@ -14,12 +15,16 @@ fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) 
     }
 });
 
+
 function makeMap(line, obj){
   var temp = line.split(";"),
       coded = temp[0].split(""),
       translated = temp[1].split("");
   
   translated.forEach(function(v,i,a){
+    if (obj[v] && (obj[v] !== coded[i] && obj[v] !== "*") ){
+      console.error("something is amiss with " + coded[i]);
+    }
     obj[v] = coded[i];
   });
   
