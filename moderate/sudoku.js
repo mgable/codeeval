65b0b313ec7b7ca160d7bd7f1873dcb1ff15a049
Mark Gable <mgable43@gmail.com>
2015-08-17 19:06:19 -0700

    sukudo

--- moderate/sudoku.js
+++ moderate/sudoku.js
@@ -8,14 +8,15 @@ fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) 
 
 function parse(line){
   var temp = line.split(";"),
-      count = temp[0],
+      count = parseInt(temp[0],10),
       matrix = temp[1];
   
-  return rowCheck(count,matrix) && columnCheck(count,matrix) ? "True" :"False" ; 
+  return rowCheck(count,matrix) && columnCheck(count,matrix) && groupCheck(count,matrix) ? "True" :"False" ; 
 }
 
 function rowCheck(count,nums){
   var isSukudo = true, matrix = nums.split(",");
+
   do {isSukudo = check(matrix.splice(0,count))}
   while (isSukudo && matrix.length);
   
@@ -23,18 +24,19 @@ function rowCheck(count,nums){
 }
 
 function columnCheck(count,nums){
-  var isSukudo = true, matrix = nums.split(","),temp = [];
+  var matrix = nums.split(","),temp = [];
   for (var x = 0; x < count; x++){
     temp = [];
     for (var i = 0; i < count; i++){
       temp.push(matrix[x + (i * count)]);
     }
-    isSukudo = check(temp);
+    if (! check(temp)) {
+      return false;
+    }
   }
   
-  return isSukudo;
+  return true;
 }
-
 
 function check(nums){
   nums.sort(function (a,b){return a-b});
@@ -44,4 +46,57 @@ function check(nums){
     }
   }
   return true; 
+}
+
+function groupCheck(count, nums){
+  var matrix = nums.split(","),
+      arrayPositions = makeGroupIndexes(count).join(",").split(","),
+      results = [],
+      isSukudo = true;
+    
+  results = arrayPositions.map(function(v,i,a){
+    return matrix[v];
+  });
+  
+  
+  do {isSukudo = check(results.splice(0,count))}
+  while (isSukudo && results.length);
+  
+  return isSukudo;
+}
+
+function makeGroupIndexes(howMany){
+  var arrays = [],
+      totalGroups = howMany,
+      factor = Math.sqrt(totalGroups),
+      currentGroup = 0,
+      currentRow = 0,
+      group = 0,
+      cell = 0;
+
+
+  while (currentRow < factor){
+    // make group(array) if necessary
+    if (!arrays[currentGroup]) arrays[currentGroup] = [];
+    arrays[currentGroup].push(cell++);
+    
+    // move to the next group
+    if (cell % Math.sqrt(totalGroups) === 0) {
+      currentGroup++;
+    }
+    
+    // reset group when end of subgroup row
+    if (cell % totalGroups === 0){
+      currentGroup = group;
+    }
+
+      // increment to new set of groups and new row
+    if (cell % Math.pow(factor,3) === 0){
+      group += factor;
+      currentGroup = group;
+      currentRow++;
+    }
+  }
+
+  return arrays;
 }
